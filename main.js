56956dc2c3060e4910f1337c1476405303e0c02b
joshua <joshua.deosaran@gmail.com>
2020-04-13 03:18:36 -0400

    append value to div on another page / fix positioning in checkout area / not working on mobile

--- main.js
+++ main.js
@@ -44,7 +44,15 @@ function addToCartClicked(event) {
     var title = shopItem.getElementsByClassName('shop-item')[0].innerText;
     var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText;
     var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src;
-    console.log(title, price, imageSrc)
+    console.log(title, price, imageSrc);
+    addItemToCart(title, price, imageSrc);
+}
+
+function addItemToCart(title, price, imageSrc) {
+    var cartRow = document.createElement('div');
+    cartRow.innerText = title
+    var cartItems = document.getElementsByClassName('product-details')[0]
+    cartItems.append(cartRow)
 }
 
 function updateCartTotal() {
